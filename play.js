c0484fc8b8a069eaf94dfa88b3359be43c69c4e9
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2022-04-20 00:05:08 -0600

    Stdin Setup

--- play.js
+++ play.js
@@ -1,3 +1,23 @@
 const { connect } = require("./client.js");
 
 connect;
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+  console.log("You pressed: ", key);
+};
+
+setupInput();

a2f181cbf6ae9a5e62041eb2bfe108e498a206ed
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2022-04-20 00:45:31 -0600

    WASD Movement

--- input.js
+++ input.js
@@ -1,6 +1,9 @@
-// setup interface to handle user input from stdin
+// Stores the active TCP connection object.
+let connection;
 
-const setupInput = function () {
+// setup interface to handle user input from stdin
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -12,8 +15,17 @@ const setupInput = function () {
 const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
-  }
-  console.log("You pressed: ", key);
+  } else if(key === 'w' || key === 'W') {
+    connection.write('Move: up');
+  } else if(key === 'a' || key === 'A') {
+    connection.write('Move: left');
+  } else if(key === 's' || key === 'S') {
+    connection.write('Move: down');
+  } else if(key === 'd' || key === 'D') {
+    connection.write('Move: right');
+  } else {
+    //Do nothing;
+  } 
 };
 
 module.exports = {

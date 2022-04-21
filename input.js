e0fbc3e2240fbbfde4ab7a651aa95a8f285bf8a7
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2022-04-20 22:47:22 -0600

    Made constants file to separate the constants

--- input.js
+++ input.js
@@ -1,7 +1,9 @@
+const { MOVE_UP_KEY, MOVE_LEFT_KEY, MOVE_DOWN_KEY, MOVE_RIGHT_KEY } = require("./constants");
+
 // Stores the active TCP connection object.
 let connection;
 let sendMessage = false;
-  let message = "Say: ";
+let message = "Say: ";
 
 // setup interface to handle user input from stdin
 const setupInput = (conn) => {
@@ -18,13 +20,13 @@ const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
   } else if(key === 'w' || key === 'W') {
-    connection.write('Move: up');
+    connection.write(MOVE_UP_KEY);
   } else if(key === 'a' || key === 'A') {
-    connection.write('Move: left');
+    connection.write(MOVE_LEFT_KEY);
   } else if(key === 's' || key === 'S') {
-    connection.write('Move: down');
+    connection.write(MOVE_DOWN_KEY);
   } else if(key === 'd' || key === 'D') {
-    connection.write('Move: right');
+    connection.write(MOVE_RIGHT_KEY);
   } else if(key === '1'){
     //If we press 1, that means we will send message
     sendMessage = true;
@@ -32,7 +34,6 @@ const handleUserInput = function(key) {
   } else if(key === '0') {
     //If we pressed 0 it will mean that message ends
     if (sendMessage) {
-      console.log("Send message");
       connection.write(message);
       message = "Say: ";
     }
@@ -40,7 +41,6 @@ const handleUserInput = function(key) {
   }
   if (sendMessage) {
     message += key;
-    console.log(message);
   }
 };
 
